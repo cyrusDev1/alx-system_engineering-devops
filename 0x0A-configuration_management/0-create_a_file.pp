015a69f7517a64735949c050481c6fa1e91c40fe
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-09-17 18:46:56 +0100

    Create file using puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,6 @@
 # Using Puppet, create a file in /tmp. 
 file { '/tmp/school':
-    ensure  => 'file'
+    ensure  => 'file',
     owner   => 'www-data',
     group   => 'www-data',
     mode    => '0744',
