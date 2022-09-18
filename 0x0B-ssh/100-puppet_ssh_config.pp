01e29940cee46f98f8182e2adbc92a22d6de40d4
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-09-18 13:36:28 +0100

    Task 4: Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,12 +3,12 @@
 # up your client SSH configuration file so that you can connect to a server
 # without typing a password.
 
-file_line { 'Turn off passwd auth'
+file_line { 'Turn off passwd auth':
     path => '/etc/ssh/ssh_config',
     line => 'PasswordAuthentication no'
 }
 
-file_line { 'Declare identity file'
+file_line { 'Declare identity file':
     path => '/etc/ssh/ssh_config',
     line => 'IdentityFile ~/.ssh/school'
 }
