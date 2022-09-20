d4c91a354d089fa59d65a2cb9825263abad98c79
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-09-20 13:40:44 +0100

    Install Nginx web server (w/ Puppet)

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -16,12 +16,28 @@ file { '/var/www/html/index.html':
     content => 'Hello World!',
 }
 
-file_line { 'configure redirection':
-    path  => '/etc/nginx/sites-available/default',
-    after => 'server_name _;',
-    line  => "\n\tlocation /redirect_me {\n\treturn 301 https://www.youtube.com/watch?v=QH2-TGUlwu4\n\t}",
+file { 'Nginx default file':
+    ensure => file,
+    path => '/etc/nginx/sites-enabled/default',
+    content => "server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+               root /var/www/html;
+        index index.html index.htm index.nginx-debian.html;
+        server_name _;
+        location / {
+                # First attempt to serve request as file, then
+                # as directory, then fall back to displaying a 404.
+                try_files \$uri \$uri/ =404;
+        }
+        
+        location /redirect_me {
+        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4
+        }
+}",
 }
 
-service { 'nginx':
-    ensure => 'running',
+exec { 'restart service':
+  command => 'service nginx restart',
+  path    => '/usr/bin:/usr/sbin:/bin',
 }
