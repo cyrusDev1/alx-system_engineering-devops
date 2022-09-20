bed6a71935f4632b699fd180652c2eabccb43310
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-09-20 14:55:02 +0100

    Install Nginx web server (w/ Puppet)

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -7,7 +7,7 @@ file_line { 'Nginx default file':
     ensure => 'present',
     path   => '/etc/nginx/sites-available/default',
     after  => 'server_name _;',
-    line   => "\n\tlocation /redirect_me {\n\treturn 301 https://www.youtube.com/watch?v=QH2-TGUlwu4\n\t}",
+    line   => "\n\tlocation /redirect_me {\n\t\treturn 301 https://www.youtube.com/watch?v=QH2-TGUlwu4\n\t}",
 }
 
 file { '/var/www/html/index.html':
