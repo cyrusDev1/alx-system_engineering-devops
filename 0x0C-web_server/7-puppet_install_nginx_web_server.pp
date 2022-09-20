2e78dfcabbbc2d2de127da602c611e388b3769be
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-09-20 14:29:32 +0100

    Install Nginx web server (w/ Puppet)

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,17 +1,37 @@
-# Puppet manifest to install nginx
-package { 'nginx':
-  ensure => installed,
+# install nginx  (w/ Puppet)
+
+exec { 'updating packages':
+    command => 'apt-get -y update',
+    path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
 
-file_line { 'redirection confg':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+package { 'nginx':
+    ensure => 'installed',
 }
 
 file { '/var/www/html/index.html':
-  content => 'Hello World!',
+    content => 'Hello World!',
+}
+
+file { 'Nginx default file':
+    ensure  => file,
+    path    => '/etc/nginx/sites-enabled/default',
+    content => "server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+               root /var/www/html;
+        index index.html index.htm index.nginx-debian.html;
+        server_name _;
+        location / {
+                # First attempt to serve request as file, then
+                # as directory, then fall back to displaying a 404.
+                try_files \$uri \$uri/ =404;
+        }
+        
+        location /redirect_me {
+        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4
+        }
+}",
 }
 
 service { 'nginx':
