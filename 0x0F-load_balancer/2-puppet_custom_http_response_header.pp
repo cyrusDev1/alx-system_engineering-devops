0a0c649ea75bb8cba7908b4e9d5ba076be5aeab7
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-09-26 18:41:46 +0100

    puppet custom http response header

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -11,11 +11,11 @@ file_line { 'Nginx default file':
     require => Package['nginx'],
 }
 
-file_line { 'Nginx http headers':
-    ensure => 'present',
-    path => '/etc/nginx/sites-available/default',
-    after => 'server_name _;',
-    line => "\n\tadd_header X-Served-By $hostname;\n",
+file_line { 'headers':
+    ensure  => 'present',
+    path    => '/etc/nginx/sites-available/default',
+    after   => 'listen 80 default_server;',
+    line    => 'add_header X-Served-By $hostname;',
     require => Package['nginx'],
 }
 
