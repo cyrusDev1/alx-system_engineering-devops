61693662cd8ebcbf7bdc0abe263ce74c2739e43e
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-10-17 22:32:07 +0100

    task 0. Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,25 +2,26 @@
 """Write a Python script that, using this REST API, for a given employee ID,
 returns information about his/her TODO list progress."""
 
+
 if __name__ == "__main__":
-    import requests
-    import sys
-    id = sys.argv[1]
-    url_user = "https://jsonplaceholder.typicode.com/users/{}".format(id)
-    url_todo = "https://jsonplaceholder.typicode.com/todos"
-    response_user = requests.get(url_user)
-    response_todo = requests.get(url_todo)
-    todo = response_todo.json()
-    name = response_user.json().get("name")
-    total = 0
-    do = 0
-    titles = []
-    for dict in todo:
-        if dict.get("userId") == int(id):
-            if dict.get("completed") is True:
-                do += 1
-                titles.append(dict.get("title"))
-            total += 1
-    print("Employee {} is done with tasks({}/{})".format(name, do, total))
-    for title in titles:
-        print(f"     {title}")
+	import requests
+	import sys
+	id = sys.argv[1]
+	url_user = "https://jsonplaceholder.typicode.com/users/{}".format(id)
+	url_todo = "https://jsonplaceholder.typicode.com/todos"
+	response_user = requests.get(url_user)
+	response_todo = requests.get(url_todo)
+	todo = response_todo.json()
+	name = response_user.json().get("name")
+	total = 0
+	do = 0
+	titles = []
+	for dict in todo:
+		if dict.get("userId") == int(id):
+			if dict.get("completed") is True:
+				do += 1
+				titles.append(dict.get("title"))
+			total += 1
+	print("Employee {} is done with tasks({}/{})".format(name, do, total))
+	for title in titles:
+		print(f"	 {title}")
