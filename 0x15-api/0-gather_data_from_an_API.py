291348fe065eeea016d690901f129c44b7192105
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-10-17 22:38:50 +0100

    task 0. Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""Write a Python script that, using this REST API, for a given employee ID,
-returns information about his/her TODO list progress."""
+"""
+Write a Python script that, using this REST API, for a given employee ID,
+returns information about his/her TODO list progress.
+"""
 
 
 if __name__ == "__main__":
-	"""Write a Python script that, using this REST API, for a given employee ID,
-	returns information about his/her TODO list progress."""
 	import requests
 	import sys
 	id = sys.argv[1]
